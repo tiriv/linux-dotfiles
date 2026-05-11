e5a7a69d4804fe12a7f09075b0abbe652a333b5d
tiriv <tiriv_@outlook.com>
2026-05-11 23:35:52 +0200

    Add files via upload

---
+++ hypr/env.lua
@@ -0,0 +1,22 @@
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+
+
+
+--theming
+hl.env("XCURSOR_SIZE", "20")
+hl.env("HYPRCURSOR_SIZE", "20")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+--hl.env("GTK_THEME", "catppuccin-mocha-mauve-standard-default")
+--nvidia 
+hl.env("GBM_BACKEND", "nvidia-drm")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+--xdg 
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+--qt
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1.25")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
---
+++ hypr/input.lua
@@ -0,0 +1,35 @@
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+    input = {
+        kb_layout  = "pl",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = true,
+        },
+    },
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace"
+})
+
+-- Example per-device config
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
+hl.device({
+    name        = "razer-razer-cobra",
+    sensitivity = -0.2,
+    accel_profile = "flat"
+})
