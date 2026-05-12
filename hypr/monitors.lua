af7a866a87df95f837a1dba8853e9e249861cbfa
tiriv <tiriv_@outlook.com>
2026-05-12 19:50:46 +0200

    Delete hypr directory

--- hypr/monitors.lua
+++
@@ -1,8 +0,0 @@
-------------------
----- MONITORS ----
-------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
-hl.monitor({ output = "", mode = "highres", position = "auto", scale = "1.25" })
-
-hl.config({ xwayland = { force_zero_scaling = true } })
